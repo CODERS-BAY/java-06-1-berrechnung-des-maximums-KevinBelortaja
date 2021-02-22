94113d6bf292d8bd3250c278f6fc863b15482952
Kevin Belortaja <kevin.belortaja@gmail.com>
2021-02-22 15:13:46 +0100

    Add exercise 6.1

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -25,7 +25,9 @@ public class Main {
         int largest = numbers[0];
 
         for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] > largest) largest = numbers[i];
+            if (numbers[i] > largest) {
+                largest = numbers[i];
+            }
         }
 
         System.out.println("The largest number is: " + largest);
